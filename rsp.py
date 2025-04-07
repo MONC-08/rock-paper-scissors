e8bc843e20e1c8dbba2da044ed09c29260e10432
Chowdhury <md.nasif008@gmail.com>
2025-04-07 09:20:04 +0200

    game logic done

--- rsp.py
+++ rsp.py
@@ -1,15 +1,29 @@
 import sys
 import random
 
+print("")
 print("Enter...\n1 for Rock,\n2 for Paper, or\n3 for Scissors.")
 playerChoice = input("Enter 1, 2 or 3\n")
-print(playerChoice)
 
 player = int(playerChoice)
-
 if(player < 1 or player >3):
     sys.exit("Enter a valid number!")
 
 computerChoice = random.choice("123")
 computer = int(computerChoice)
-print(computerChoice)
+
+print("")
+print("You chose:", player)
+print("Python Chose:", computer)
+print("")
+
+if player == 1 and computer == 3:
+    print("You win!")
+elif player == 2 and computer == 1:
+    print("You win!")
+elif player == 3 and computer == 2:
+    print("You win!")
+elif player == computer:
+    print("It\'s a tie!")
+else:
+    print("Python wins!")
