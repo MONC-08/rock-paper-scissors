efe94e180d07fc936e27e05f939c421ce9acf886
Chowdhury <md.nasif008@gmail.com>
2025-04-07 09:08:30 +0200

    random value picked

--- rsp.py
+++ rsp.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 print("Enter...\n1 for Rock,\n2 for Paper, or\n3 for Scissors.")
 playerChoice = input("Enter 1, 2 or 3\n")
@@ -8,3 +9,7 @@ player = int(playerChoice)
 
 if(player < 1 or player >3):
     sys.exit("Enter a valid number!")
+
+computerChoice = random.choice("123")
+computer = int(computerChoice)
+print(computerChoice)
